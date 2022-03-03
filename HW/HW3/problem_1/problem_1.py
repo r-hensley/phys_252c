94f9e129af3e88aa950012671e808ddde8915956
Ryry013 <ryry013@yahoo.com>
2022-03-02 23:24:59 -0800

    HW1 Problem 3 Solution (needs cleanup)

--- HW/HW3/problem_1/problem_1.py
+++ HW/HW3/problem_1/problem_1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 # 1. Using the resonance spectrum data from the previous homework assignment, apply a
 # Bayesian likelihood method to obtain a posterior pdf in the signal cross section, based
@@ -40,16 +42,29 @@ def log_likelihood(_n, _bins, _s):
     return -log_likelihood_sum
 
 
-def integrate_likelihood(_x, _y, _low_bin, _high_bin):
+def integrate_likelihood(_x, _y, _low_x, _high_x):
     dx = _x[1] - _x[0]
-
     int_sum = 0
-    for i in range(_low_bin, _high_bin+1):
-        current_bin = _low_bin + dx * i
-        int_sum += dx * _y[current_bin]
+    for i, y in enumerate(_y):
+        if not _low_x <= _x[i] <= _high_x:
+            continue
+        int_sum += dx * y
+    return int_sum
 
 
-with open("../../Data/resonance.dat", "r") as f:
+def find_bound(_x, _y, backwards=False):  # 15.865
+    dx = _x[1] - _x[0]
+    int_sum = 0
+    if backwards:
+        _x = np.flip(_x)
+        _y = np.flip(_y)
+    for i, y in enumerate(_y):
+        int_sum += dx * y
+        if int_sum > 0.15865:
+            return _x[i]
+
+
+with open("../Data/resonance.dat", "r") as f:
     data = f.read().replace(' ', '').splitlines()
 
 data = [float(i) for i in data]
@@ -57,8 +72,8 @@ fig, ax = plt.subplots()
 
 n, bins, patches = ax.hist(data, 36, (100, 1000), label="Resonance data")
 
-precision = 5
-max_s = 5000
+precision = 0.1
+max_s = 2500
 number_of_bins = int(max_s/precision + 1)
 likelihood_list_x = np.array([])
 likelihood_list_y = np.array([])
@@ -72,6 +87,12 @@ for i, s in enumerate(np.linspace(0, max_s, number_of_bins)):
     likelihood_list_x = np.append(likelihood_list_x, s)
     likelihood_list_y = np.append(likelihood_list_y, next_likelihood - base_likelihood)
 
+likelihood_list_y = np.exp(likelihood_list_y)
+likelihood_magnitude = integrate_likelihood(likelihood_list_x, likelihood_list_y, 0, likelihood_list_x[-1])
+likelihood_list_y = likelihood_list_y / likelihood_magnitude
+lower_bound = find_bound(likelihood_list_x, likelihood_list_y, backwards=False)
+upper_bound = find_bound(likelihood_list_x, likelihood_list_y, backwards=True)
+print(f"{lower_bound=} --- {upper_bound=}")
 # print(f"{likelihood_list_x=}")
 # print(f"{likelihood_list_y=}")
 
@@ -87,12 +108,39 @@ for i, j in enumerate(likelihood_list_x):
 
 print(f"Bin {max_bin}:\n{likelihood_list_x[max_bin]}: {likelihood_list_y[max_bin]}")
 
-fig2, ax2 = plt.subplots()
-ax2.plot(likelihood_list_x, likelihood_list_y)
 
-# max_bin=184
+fig2, ax2 = plt.subplots()
+fig2: Figure
+ax2: Axes
+ax2.plot(likelihood_list_x, likelihood_list_y)
+ax2.set_title("Posterior PDF from likelihood fit with 68.27% window")
+ax2.set_xlabel("s estimator")
+ax2.set_ylabel("Probability")
+ax2.fill_between(likelihood_list_x,
+                 likelihood_list_y,
+                 where=(lower_bound < likelihood_list_x) & (likelihood_list_x < upper_bound),
+                 alpha=0.5)
+plt.xlim(686, 1150)
+ax2.text(0.05, 0.95, f"Lower bound: {lower_bound}\nUpper bound: {upper_bound}",
+         transform=ax2.transAxes, fontsize=12, verticalalignment="top")
+# Lower bound: 866, Upper bound: 972.7
+
+max_s = likelihood_list_x[max_bin]
+
+ax2.text(0.60, 0.95, rf"Estimator: ${max_s}^{{+{round(upper_bound-max_s, 1)}}}_{{-{round(max_s-lower_bound, 1)}}}$",
+         transform=ax2.transAxes, fontsize=12, verticalalignment="top")
+# 918.2 + 54.5 - 52.2
+_, top = plt.ylim()
+bottom = 0
+plt.ylim(bottom=bottom)
+y_percentage = (likelihood_list_y[max_bin] - bottom) / (top - bottom)
+ax2.axvline(max_s, 0, y_percentage, color="#FF0000")
+
+
+# lower_bound=866.0 --- upper_bound=972.7
+# max_bin=9182
 # Likelihoods
-# Bin 184:
-# 920.0: 197.54595161386533
+# Bin 9182:
+# 918.2: 0.007474625108787476
 
 plt.show()
