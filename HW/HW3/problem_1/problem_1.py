5116d2694b0a910baa41e8fa590282160b8ca3f2
Ryry013 <ryry013@yahoo.com>
2022-03-03 19:12:51 -0800

    HW3 Problem 1 Final

--- HW/HW3/problem_1/problem_1.py
+++ HW/HW3/problem_1/problem_1.py
@@ -27,19 +27,12 @@ def gint(mu, sigma, xlow, xhigh):
     return int_sum * dx / (sigma * ROOT2PI)
 
 
-def log_likelihood(_n, _bins, _s):
-    # print(f"{_s=}")
+def calc_log_likelihood(_n, _bins, _s):
     log_likelihood_sum = 0
     for bin_number, bin_value in enumerate(_n):
-        # print(f"{_bins[bin_number]=}")
-        # print(f"{_bins[bin_number+1]=}")
-        # print(f"{bin_number=}")
-        # print(f"{bin_value=}")
         mu = 251.188 + _s * gint(MU, SIGMA, _bins[bin_number], _bins[bin_number+1])
-        # print(f"{mu=}")
-        # print("")
-        log_likelihood_sum += mu - bin_value * np.log(mu)
-    return 2*log_likelihood_sum
+        log_likelihood_sum += bin_value * np.log(mu) - mu
+    return log_likelihood_sum
 
 
 def integrate_likelihood(_x, _y, _low_x, _high_x):
@@ -60,8 +53,7 @@ def find_bound(_x, _y, backwards=False):  # 15.865
         _y = np.flip(_y)
     for i, y in enumerate(_y):
         int_sum += dx * y
-        print(int_sum)
-        if int_sum < -0.15865:
+        if int_sum > 0.15865:
             return _x[i]
 
 
@@ -73,77 +65,73 @@ fig, ax = plt.subplots()
 
 n, bins, patches = ax.hist(data, 36, (100, 1000), label="Resonance data")
 
-precision = 5
+precision = 0.5
 max_s = 2500
 number_of_bins = int(max_s/precision + 1)
-likelihood_list_x = np.array([])
-likelihood_list_y = np.array([])
-base_likelihood = 0
-for i, s in enumerate(np.linspace(0, max_s, number_of_bins)):
-    next_likelihood = log_likelihood(n, bins, s)
-    if s == 0:
-        base_likelihood = next_likelihood
-    print(f"{s=}, {base_likelihood=}, {next_likelihood=}")
-    if next_likelihood - base_likelihood > 0:
-        break
-    likelihood_list_x = np.append(likelihood_list_x, s)
-    likelihood_list_y = np.append(likelihood_list_y, next_likelihood - base_likelihood)
+s_values = np.linspace(0, max_s, number_of_bins)  # [0, 0.5, 1.0, 1.5, ..., 2499.05, 2500]
+log_likelihood_list_y = np.array([])
 
-likelihood_list_y = np.exp(likelihood_list_y)
-likelihood_magnitude = integrate_likelihood(likelihood_list_x, likelihood_list_y, 0, likelihood_list_x[-1])
+base_likelihood = calc_log_likelihood(n, bins, 0)  # Will subtract the s=0 likelihood as an offset
+for i, s in enumerate(s_values):
+    log_likelihood = calc_log_likelihood(n, bins, s) - base_likelihood
+    if log_likelihood < 0:
+        break  # Stop adding values to array when likelihood becomes negative
+    log_likelihood_list_y = np.append(log_likelihood_list_y, log_likelihood)
+
+# Cut off end of x-array to match size of y-array
+s_values = s_values[0 : len(log_likelihood_list_y)]
+
+# Exponentiate and normalize
+likelihood_list_y = np.exp(log_likelihood_list_y)
+likelihood_magnitude = integrate_likelihood(s_values, likelihood_list_y, 0, s_values[-1])
 likelihood_list_y = likelihood_list_y / likelihood_magnitude
-lower_bound = find_bound(likelihood_list_x, likelihood_list_y, backwards=False)
-upper_bound = find_bound(likelihood_list_x, likelihood_list_y, backwards=True)
-print(f"{lower_bound=} --- {upper_bound=}")
-# print(f"{likelihood_list_x=}")
-# print(f"{likelihood_list_y=}")
 
-# print(f"{max(likelihood_list[1])=}")
-max_bin = np.where(likelihood_list_y == min(likelihood_list_y))[0][0]
-print(f"{max_bin=}")
+# Find lower and upper bounds of 67% confidence interval
+lower_bound = find_bound(s_values, likelihood_list_y, backwards=False)  # Lower bound of 67% interval (x-value)
+upper_bound = find_bound(s_values, likelihood_list_y, backwards=True)  # Upper bound of 67% interval (x-value)
 
+max_bin = np.where(likelihood_list_y == max(likelihood_list_y))[0][0]  # Bin number of maximum bin
+max_x = s_values[max_bin]  # X-value for maximum bin
 
-print("Likelihoods")
-for i, j in enumerate(likelihood_list_x):
-    # print(f"{likelihood_list_x[i]}: {likelihood_list_y[i]}")
-    pass
-
-print(f"Bin {max_bin}:\n{likelihood_list_x[max_bin]}: {likelihood_list_y[max_bin]}")
-
-
+# Plot posterior PDF
 fig2, ax2 = plt.subplots()
-fig2: Figure
-ax2: Axes
-ax2.plot(likelihood_list_x, likelihood_list_y)
+fig2: Figure  # Typehinting
+ax2: Axes  # Typehinting
+ax2.plot(s_values, likelihood_list_y)
 ax2.set_title("Posterior PDF from likelihood fit with 68.27% window")
 ax2.set_xlabel("s estimator")
 ax2.set_ylabel("Probability")
-ax2.fill_between(likelihood_list_x,
+plt.xlim(686, 1150)  # Center view around peak
+
+# Fill in 67% confidence range
+ax2.fill_between(s_values,
                  likelihood_list_y,
-                 where=(lower_bound < likelihood_list_x) & (likelihood_list_x < upper_bound),
+                 where=(lower_bound < s_values) & (s_values < upper_bound),
                  alpha=0.5)
-# plt.xlim(686, 1150)
-plt.xlim(0, 2000)
+
+# Add text for bounds and estimator value
 ax2.text(0.05, 0.95, f"Lower bound: {lower_bound}\nUpper bound: {upper_bound}",
          transform=ax2.transAxes, fontsize=12, verticalalignment="top")
-# Lower bound: 866, Upper bound: 972.7
-
-max_s = likelihood_list_x[max_bin]
-
-ax2.text(0.60, 0.95, rf"Estimator: ${max_s}^{{+{round(upper_bound-max_s, 1)}}}_{{-{round(max_s-lower_bound, 1)}}}$",
+ax2.text(0.60, 0.95, rf"Estimator: ${max_x}^{{+{round(upper_bound-max_x, 1)}}}_{{-{round(max_x-lower_bound, 1)}}}$",
          transform=ax2.transAxes, fontsize=12, verticalalignment="top")
-# 918.2 + 54.5 - 52.2
-bottom, _ = plt.ylim()
-top = 0
-plt.ylim(top=top)
-y_percentage = (likelihood_list_y[max_bin] - top) / (top - bottom)
-ax2.axvline(max_s, 1-y_percentage, 1, color="#FF0000")
+
+# Add vertical line marking maximum
+_, top = plt.ylim()
+plt.ylim(bottom=0)
+y_percentage = likelihood_list_y[max_bin] / top
+ax2.axvline(max_x, 0, y_percentage, color="#FF0000")
 
 
-# lower_bound=866.0 --- upper_bound=972.7
-# max_bin=9182
-# Likelihoods
-# Bin 9182:
-# 918.2: 0.007474625108787476
+# #############################################
+# ################## RESULTS ##################
+# Precision=0.5
+# max_bin=1818
+# Bin 1818:
+# 909.0: 0.007478422884021669
+# Lower bound: 857, Upper bound: 963.5
+# Estimator: 909 + 54.5 - 52.0
+# #############################################
+# #############################################
+
 
 plt.show()
